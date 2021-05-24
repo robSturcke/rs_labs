239520f05c5e1c43f92ef920efdf68f267708289
Rob Sturcke <rob@rslabs.dev>
2021-05-24 09:40:59 -0400

    fixes `node-sass` error with CRA
    adds basic routing template

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import About from './pages/about';
+
 function App() {
   return (
-    <div className="App">
+    <Router>
       <header className="App-header">RSLabs Dashboard</header>
-    </div>
+      <Link to="/about">About</Link>
+      <Switch>
+        <Route path="/about">
+          <About />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
